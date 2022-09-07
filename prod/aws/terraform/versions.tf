b3264e8a6163143029c2a7980d6f29b5e9ba471c
A B Vijay Kumar <6584323+abvijaykumar@users.noreply.github.com>
2022-09-07 18:45:38 +0530

    added terraform code

--- prod/aws/terraform/versions.tf
+++ prod/aws/terraform/versions.tf
@@ -2,14 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.15.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "3.1.0"
+      version = "~> 4.0"
     }
   }
-
-  required_version = "~> 1.2.0"
 }

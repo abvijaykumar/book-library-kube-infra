4862ab565c068562f381e1ef04deabcd50c24b8a
A B Vijay Kumar <6584323+abvijaykumar@users.noreply.github.com>
2022-09-19 12:47:20 +0530

    Cleaned up

--- dev/dependencies-tf/modules/vault/main.tf
+++
@@ -1,9 +0,0 @@
-provider "vault" {
-  # It is strongly recommended to configure this provider through the
-  # environment variables:
-  #    - VAULT_ADDR
-  #    - VAULT_TOKEN
-  #    - VAULT_CACERT
-  #    - VAULT_CAPATH
-  #    - etc.
-}

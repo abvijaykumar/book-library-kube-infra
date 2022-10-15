4c08e57a4b31f53247ec762b0cf244a009a972a9
A B Vijay Kumar <6584323+abvijaykumar@users.noreply.github.com>
2022-10-15 12:45:44 +0530

    Fxied issues

--- dev/helmfiles/setup_infra.sh
+++ dev/helmfiles/setup_infra.sh
@@ -8,18 +8,15 @@ helm repo add hashicorp https://helm.releases.hashicorp.com
 
 helm repo update
 
-helm install prometheus prometheus-community/kube-prometheus-stack -n monitoring
-
-kubectl patch ds -n monitoring prom-stack-prometheus-node-exporter --type "json" -p '[{"op": "remove", "path" : "/spec/template/spec/containers/0/volumeMounts/2/mountPropagation"}]'
-
-
+helm install -n monitoring --create-namespace prometheus prometheus-community/kube-prometheus-stack 
 helm install --create-namespace -n monitoring redis-exporter prometheus-community/prometheus-redis-exporter --values ./values/redis-exporter-helm-values.yaml
 helm install --create-namespace -n monitoring postgres-exporter prometheus-community/prometheus-postgres-exporter --values ./values/postgres-exporter-helm-values.yaml
 helm install --create-namespace -n monitoring loki grafana/loki-stack --values ./values/loki-stack-values.yaml
-
 helm install --create-namespace -n argocd argocd/argo-cd
 helm install --create-namespace -n ingress-nginx ingress-nginx ingress-nginx/ingress-nginx
 helm install --create-namespace -n cert-manager cert-manager jetstack/cert-manager --values ./values/cert-manager-values.yaml
+helm install --create-namespace -n vault vault hashicorp/vault   --values ./values/vault-raft-values.yaml
 
-helm install vault hashicorp/vault -n vault --create-namespace --values ./values/vault-raft-values.yaml
+kubectl patch ds -n monitoring prometheus-prometheus-node-exporter --type "json" -p '[{"op": "remove", "path" : "/spec/template/spec/containers/0/volumeMounts/2/mountPropagation"}]'
 
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.3.0/deploy/static/provider/cloud/deploy.yaml
